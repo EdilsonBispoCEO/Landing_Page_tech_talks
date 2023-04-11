8d5303cf014fe33e1bf4b01be4c09d8cc52c5406
Edilson Bispo <edilson_pixot@hotmail.com>
2023-04-11 19:34:05 -0300

    tarefas feitas

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataDoEvento = new Date("Mayc 22, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMms = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMms);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMms) / 1000);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  /*  if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);                                           Caso o evento já TENH ocorrIDO, com essa linha de codigo ele mosta que EXPIROU.
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    } */
+
+}, 1000);
